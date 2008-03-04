9453258a2562ce1e92954dc3aae7f8362ab25187
Nick Palmer <nick@sluggardy.net>
2008-03-04 16:11:46 +0000

    Fix stupid MYSQL only date calculation.

--- bad-behavior/housekeeping.inc.php
+++ bad-behavior/housekeeping.inc.php
@@ -3,7 +3,7 @@
 function bb2_housekeeping($settings, $package)
 {
 	// FIXME Yes, the interval's hard coded (again) for now.
-	$query = "DELETE FROM `" . $settings['log_table'] . "` WHERE `date` < DATE_SUB('" . bb2_db_date() . "', INTERVAL 7 DAY)";
+	$query = "DELETE FROM `" . $settings['log_table'] . "` WHERE `date` < " . (bb2_db_date() - (60 * 60 * 24 * 7));
 	bb2_db_query($query);
 
 	// Waste a bunch more of the spammer's time, sometimes.
